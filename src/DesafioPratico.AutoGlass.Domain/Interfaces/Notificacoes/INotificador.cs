f394c0b0fdd8d4e817c13ff2524008af62c8270f
Lucas Kranz <kranz12@gmail.com>
2023-02-04 00:32:29 -0300

    Criadas algumas classes que serão utilizadas para Notificar ao usuário algumas mensagens de erro entre outros

---
+++ src/DesafioPratico.AutoGlass.Domain/Interfaces/Notificacoes/INotificador.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPratico.AutoGlass.Service.Notificacoes;
+
+namespace DesafioPratico.AutoGlass.Service.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
